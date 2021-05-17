9ba06e6112e5097b6130df33d254018cb57cd7b8
coffeyxyz <coffeyxyz@gmail.com>
2021-05-17 21:19:46 -0230

    Replace magic number with macro

--- src/boid.c
+++ src/boid.c
@@ -7,6 +7,8 @@
 #include "boid.h"
 #include "screen.h"
 
+#define ROTATED_SQUARE_OFFSET	0.7071f
+
 #define deg_to_rad(X)	(double)((X) * M_PI / 180.0)
 #define rad_to_deg(X)	(double)((X) * 180.0 / M_PI)
 
@@ -18,14 +20,14 @@ void boid_update(boid_t boids[], size_t boid_count)
 		boid->x += boid->velocity * (float)cos(deg_to_rad(boid->angle));
 		boid->y += boid->velocity * (float)sin(deg_to_rad(boid->angle));
 
-		if (boid->x < -boid_sprite_w * 0.7071f)
-			boid->x = (WIN_WIDTH-1) + boid_sprite_w * 0.7071f;
-		else if (boid->x >= WIN_WIDTH + boid_sprite_w * 0.7071f)
-			boid->x = -boid_sprite_w * 0.7071f;
+		if (boid->x < -boid_sprite_w * ROTATED_SQUARE_OFFSET)
+			boid->x = (WIN_WIDTH-1) + boid_sprite_w * ROTATED_SQUARE_OFFSET;
+		else if (boid->x >= WIN_WIDTH + boid_sprite_w * ROTATED_SQUARE_OFFSET)
+			boid->x = -boid_sprite_w * ROTATED_SQUARE_OFFSET;
 
-		if (boid->y < -boid_sprite_h * 0.7071f)
-			boid->y = (WIN_HEIGHT-1) + boid_sprite_h * 0.7071f;
-		else if (boid->y >= WIN_HEIGHT + boid_sprite_h * 0.7071f)
-			boid->y = -boid_sprite_h * 0.7071f;
+		if (boid->y < -boid_sprite_h * ROTATED_SQUARE_OFFSET)
+			boid->y = (WIN_HEIGHT-1) + boid_sprite_h * ROTATED_SQUARE_OFFSET;
+		else if (boid->y >= WIN_HEIGHT + boid_sprite_h * ROTATED_SQUARE_OFFSET)
+			boid->y = -boid_sprite_h * ROTATED_SQUARE_OFFSET;
 	}
 }
