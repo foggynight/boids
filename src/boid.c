7b2b53f0327ff00d455833fb59c55b40f4a87941
coffeyxyz <coffeyxyz@gmail.com>
2021-05-16 23:43:52 -0230

    Flip direction of rotation of the boid angle

--- src/boid.c
+++ src/boid.c
@@ -13,6 +13,6 @@ void boid_update(boid_t boids[], size_t boid_count)
 {
 	for (size_t i = 0; i < boid_count; ++i) {
 		boids[i].x += boids[i].velocity * (float)cos(deg_to_rad(boids[i].angle));
-		boids[i].y -= boids[i].velocity * (float)sin(deg_to_rad(boids[i].angle));
+		boids[i].y += boids[i].velocity * (float)sin(deg_to_rad(boids[i].angle));
 	}
 }
--- src/boid.h
+++ src/boid.h
@@ -9,7 +9,7 @@
 typedef struct boid {
 	float x;	// x position
 	float y;	// y position
-	int angle;	// Angle in degrees -- Relative to the right facing x-axis with a counter-clockwise rotation
+	int angle;	// Angle in degrees -- Relative to the right facing x-axis with a clockwise rotation
 	float velocity;	// Forward velocity
 } boid_t;
 
