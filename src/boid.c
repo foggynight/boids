eb25dedc1f236babdabe7a6c63a65823d3ff1b49
coffeyxyz <coffeyxyz@gmail.com>
2021-05-16 23:39:38 -0230

    Convert boid angle member from radians to degrees

--- src/boid.c
+++ src/boid.c
@@ -6,10 +6,13 @@
 
 #include "boid.h"
 
+#define deg_to_rad(X)	(double)((X) * M_PI / 180.0)
+#define rad_to_deg(X)	(double)((X) * 180.0 / M_PI)
+
 void boid_update(boid_t boids[], size_t boid_count)
 {
 	for (size_t i = 0; i < boid_count; ++i) {
-		boids[i].x += boids[i].velocity * (float)cos(boids[i].angle);
-		boids[i].y -= boids[i].velocity * (float)sin(boids[i].angle);
+		boids[i].x += boids[i].velocity * (float)cos(deg_to_rad(boids[i].angle));
+		boids[i].y -= boids[i].velocity * (float)sin(deg_to_rad(boids[i].angle));
 	}
 }
