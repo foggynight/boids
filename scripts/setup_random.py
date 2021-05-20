9fe43972022ebfc823f7aca19805d6054a525ae5
coffeyxyz <coffeyxyz@gmail.com>
2021-05-19 23:58:12 -0230

    Refactor setup scripts

--- scripts/setup_random.py
+++ scripts/setup_random.py
@@ -7,21 +7,24 @@ from random import randrange
 
 from setup_utils import *
 
-SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 720
-
 def gen_random_boid():
     x = randrange(SCREEN_WIDTH)
     y = randrange(SCREEN_HEIGHT)
-    angle = randrange(360)
-    velocity = 1
+    angle = randrange(MAX_ANGLE)
+    velocity = DEFAULT_VELOCITY
     return f'{x}.0,{y}.0,{angle}.0,{velocity}.0\n'
 
 if __name__ == '__main__':
-    number_of_boids = 16
-    setup_file = open_new_setup_file()
+    random_setup_files = sorted(glob.glob(SETUP_PATH + 'random-*.csv'), key=sort_by_title_number)
 
-    for i in range(number_of_boids):
+    if len(random_setup_files) == 0:
+        next_number = 0
+    else:
+        next_number = int(random_setup_files[-1].split('-')[1].split('.')[0]) + 1
+
+    setup_file = setup_open_new(f'random-{next_number}')
+
+    for i in range(BOID_COUNT):
         setup_file.write(gen_random_boid())
 
     setup_file.close()
