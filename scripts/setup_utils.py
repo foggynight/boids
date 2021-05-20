a7fb979dfbd60c3419bc4d53dae1067dd68560aa
foggynight <foggynight709@gmail.com>
2021-05-19 23:36:22 -0230

    Add check for correct working directory

--- scripts/setup_utils.py
+++ scripts/setup_utils.py
@@ -4,6 +4,7 @@
 # Released under the GPLv2 license
 
 import glob
+import os
 
 setup_path = '../res/setups/'
 
@@ -11,6 +12,9 @@ def sort_by_title_number(element):
     return int(element.split('-')[1].split('.')[0])
 
 def open_new_setup_file():
+    if os.getcwd().split('/')[-1] != 'scripts':
+        exit("Invalid usage: Working directory must be '.../boids/scripts'")
+
     random_setup_files = sorted(glob.glob(setup_path + 'random-*.csv'), key=sort_by_title_number)
 
     if len(random_setup_files) == 0:
