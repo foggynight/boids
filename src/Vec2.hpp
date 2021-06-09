700688a9b39799baf85e0a218a7a8fc9bc4c24f2
foggynight <foggynight709@gmail.com>
2021-06-09 12:47:55 -0230

    Replace util functions with Vec2 member functions

--- src/Vec2.hpp
+++ src/Vec2.hpp
@@ -11,10 +11,26 @@ public:
 
 public:
 	Vec2(float x = 0.0f, float y = 0.0f);
+
 	Vec2 operator+(const Vec2& target);
 	Vec2 operator-(const Vec2& target);
 	Vec2 operator*(float target);
 	Vec2 operator/(float target);
+
+	/**
+	 * Get the angle of the vector relative to the right facing x-axis, with a
+	 * clockwise rotation starting at the x-axis.
+	 *
+	 * @return Angle of the vector
+	 **/
+	float get_angle();
+
+	/**
+	 * Get the length of the vector.
+	 *
+	 * @return Length of the vector
+	 **/
+	float get_length();
 };
 
 #endif	// VEC2_HPP
