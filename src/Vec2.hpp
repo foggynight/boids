a12d6b5ba8464a590b8c34205e20b491e1a2b369
foggynight <foggynight709@gmail.com>
2021-06-09 13:09:49 -0230

    Add operator overloads and update member functions

--- src/Vec2.hpp
+++ src/Vec2.hpp
@@ -12,25 +12,30 @@ public:
 public:
 	Vec2(float x = 0.0f, float y = 0.0f);
 
-	Vec2 operator+(const Vec2& target);
-	Vec2 operator-(const Vec2& target);
-	Vec2 operator*(float target);
-	Vec2 operator/(float target);
-
 	/**
 	 * Get the angle of the vector relative to the right facing x-axis, with a
 	 * clockwise rotation starting at the x-axis.
 	 *
 	 * @return Angle of the vector
 	 **/
-	float get_angle();
+	float angle() const;
 
 	/**
 	 * Get the length of the vector.
 	 *
 	 * @return Length of the vector
 	 **/
-	float get_length();
+	float length() const;
+
+	Vec2 operator+(const Vec2& target) const;
+	Vec2 operator-(const Vec2& target) const;
+	Vec2 operator*(float target) const;
+	Vec2 operator/(float target) const;
+
+	Vec2& operator+=(const Vec2& target);
+	Vec2& operator-=(const Vec2& target);
+	Vec2& operator*=(float target);
+	Vec2& operator/=(float target);
 };
 
 #endif	// VEC2_HPP
