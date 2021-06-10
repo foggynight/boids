e893dfe9faaa4d70e0b6c20adaec6b44238fa619
foggynight <foggynight709@gmail.com>
2021-06-10 17:32:19 -0230

    Add Vec2 constructors
    Replaced the default argument constructor with three explicit
    constructors.
    
    - Default constructor which sets x and y to 0
    - Constructor to convert an angle into a unit vector
    - Constructor which takes x and y values directly

--- src/Vec2.cpp
+++ src/Vec2.cpp
@@ -9,6 +9,12 @@ extern "C" {
 #include "util.h"
 }
 
+Vec2::Vec2()
+	: x(0.0f), y(0.0f) {}
+
+Vec2::Vec2(float angle)
+	: x(cos(deg_to_rad(angle))), y(sin(deg_to_rad(angle))) {}
+
 Vec2::Vec2(float x, float y)
 	: x(x), y(y) {}
 
