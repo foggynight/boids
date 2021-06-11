1b51be0389f62d0b0995103b1b39bf0c15aa4d95
foggynight <foggynight709@gmail.com>
2021-06-11 04:56:53 -0230

    Add approx_equal module

---
+++ tests/approx_equal.h
@@ -0,0 +1,17 @@
+// Copyright (C) 2021 Robert Coffey
+// Released under the GPLv2 license
+
+#ifndef APPROX_EQUAL_H
+#define APPROX_EQUAL_H
+
+/**
+ * Determine if two floats are approximately equal.
+ *
+ * @param x	First float
+ * @param y	Second float
+ *
+ * @return 1 if floats are approximately equal else 0
+ **/
+int approx_equal(float x, float y);
+
+#endif	// APPROX_EQUAL_H
