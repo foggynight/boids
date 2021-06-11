d34f47da5eae7ad67675c6f292193a80168d30ba
foggynight <foggynight709@gmail.com>
2021-06-11 04:57:05 -0230

    Add test_Vec2
    I've only added the tests for the to_unit_vector member function thus
    far.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -6,7 +6,8 @@
 # Compile C tests
 
 # Compile C++ tests
-g++ -o test_Entity.out -I../src test_Entity.cpp ../src/Entity.cpp ../src/Vec2.cpp -lm
+g++ -o test_Entity.out -I../src test_Entity.cpp approx_equal.c ../src/util.c ../src/Entity.cpp ../src/Vec2.cpp -lm
+g++ -o test_Vec2.out -I../src test_Vec2.cpp approx_equal.c ../src/Vec2.cpp -lm
 
 # Run and delete all tests
 for TEST in *.out; do
