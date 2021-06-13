aa317a6c847792857c653e35eccce26bd1515ba1
coffeyxyz <coffeyxyz@gmail.com>
2021-06-12 22:03:33 -0230

    Fix indentation -- Somehow hadn't noticed this

--- src/util.c
+++ src/util.c
@@ -9,17 +9,17 @@ extern "C" {
 
 int find_shorter_rotation(float start_angle, float end_angle)
 {
-		float cw_angle, ccw_angle;
-		if (start_angle <= end_angle) {
-			cw_angle = end_angle - start_angle;
-			ccw_angle = 360.0f - end_angle + start_angle;
-		}
-		else {	// start_angle > end_angle
-			cw_angle = 360.0f - start_angle + end_angle;
-			ccw_angle = start_angle - end_angle;
-		}
+	float cw_angle, ccw_angle;
+	if (start_angle <= end_angle) {
+		cw_angle = end_angle - start_angle;
+		ccw_angle = 360.0f - end_angle + start_angle;
+	}
+	else {	// start_angle > end_angle
+		cw_angle = 360.0f - start_angle + end_angle;
+		ccw_angle = start_angle - end_angle;
+	}
 
-		return cw_angle > ccw_angle ? -1 : 1;
+	return cw_angle > ccw_angle ? -1 : 1;
 }
 
 #ifdef __cplusplus
