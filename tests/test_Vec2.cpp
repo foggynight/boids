12f8656a6961f6074f422bc78a154a314d50113b
coffeyxyz <coffeyxyz@gmail.com>
2021-06-12 23:24:32 -0230

    Replace to_unit_vector with normalize

--- tests/test_Vec2.cpp
+++ tests/test_Vec2.cpp
@@ -11,16 +11,16 @@ extern "C" {
 #include "approx_equal.h"
 }
 
-void test_to_unit_vector();
+void test_normalize();
 
 int main()
 {
-	test_to_unit_vector();
+	test_normalize();
 	std::cout << "test_Vec2: All tests passed" << std::endl;
 	return 0;
 }
 
-void test_to_unit_vector()
+void test_normalize()
 {
 	/**
 	 * Test case format: input_x, input_y, expected_x, expected_y
@@ -29,63 +29,63 @@ void test_to_unit_vector()
 	Vec2 vec(0.0f, 0.0f);
 
 	// Test 0: 0, 0, 1, 0
-	vec.to_unit_vector();
+	vec.normalize();
 	assert(approx_equal(vec.x, 1.0f));
 	assert(approx_equal(vec.y, 0.0f));
 
 	// Test 1: 2, 0, 1, 0
 	vec.x = 2.0f;
 	vec.y = 0.0f;
-	vec.to_unit_vector();
+	vec.normalize();
 	assert(approx_equal(vec.x, 1.0f));
 	assert(approx_equal(vec.y, 0.0f));
 
 	// Test 2: 0, 2, 0, 1
 	vec.x = 0.0f;
 	vec.y = 2.0f;
-	vec.to_unit_vector();
+	vec.normalize();
 	assert(approx_equal(vec.x, 0.0f));
 	assert(approx_equal(vec.y, 1.0f));
 
 	// Test 3: -2, 0, -1, 0
 	vec.x = -2.0f;
 	vec.y = 0.0f;
-	vec.to_unit_vector();
+	vec.normalize();
 	assert(approx_equal(vec.x, -1.0f));
 	assert(approx_equal(vec.y, 0.0f));
 
 	// Test 4: 0, -2, 0, -1
 	vec.x = 0.0f;
 	vec.y = -2.0f;
-	vec.to_unit_vector();
+	vec.normalize();
 	assert(approx_equal(vec.x, 0.0f));
 	assert(approx_equal(vec.y, -1.0f));
 
 	// Test 5: 1, 1, 1/sqrt(2), 1/sqrt(2)
 	vec.x = 1.0f;
 	vec.y = 1.0f;
-	vec.to_unit_vector();
+	vec.normalize();
 	assert(approx_equal(vec.x, (float)1/sqrt(2.0)));
 	assert(approx_equal(vec.y, (float)1/sqrt(2.0)));
 
 	// Test 6: -1, 1, -1/sqrt(2), 1/sqrt(2)
 	vec.x = -1.0f;
 	vec.y = 1.0f;
-	vec.to_unit_vector();
+	vec.normalize();
 	assert(approx_equal(vec.x, (float)-1/sqrt(2.0)));
 	assert(approx_equal(vec.y, (float)1/sqrt(2.0)));
 
 	// Test 7: -1, -1, -1/sqrt(2), -1/sqrt(2)
 	vec.x = -1.0f;
 	vec.y = -1.0f;
-	vec.to_unit_vector();
+	vec.normalize();
 	assert(approx_equal(vec.x, (float)-1/sqrt(2.0)));
 	assert(approx_equal(vec.y, (float)-1/sqrt(2.0)));
 
 	// Test 8: 1, -1, 1/sqrt(2), -1/sqrt(2)
 	vec.x = 1.0f;
 	vec.y = -1.0f;
-	vec.to_unit_vector();
+	vec.normalize();
 	assert(approx_equal(vec.x, (float)1/sqrt(2.0)));
 	assert(approx_equal(vec.y, (float)-1/sqrt(2.0)));
 }
