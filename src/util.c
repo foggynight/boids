652c3d6003bfa383cea3440dfb97baf650cd295b
foggynight <foggynight709@gmail.com>
2021-06-12 22:12:12 -0230

    Add no rotation case and CW, CCW, NO_ROT macros

--- src/util.c
+++ src/util.c
@@ -7,19 +7,26 @@ extern "C" {
 
 #include "util.h"
 
+#define CW	1	// Clockwise
+#define CCW	-1	// Counter clockwise
+#define NO_ROT	0	// No rotation
+
 int find_shorter_rotation(float start_angle, float end_angle)
 {
 	float cw_angle, ccw_angle;
-	if (start_angle <= end_angle) {
+	if (start_angle < end_angle) {
 		cw_angle = end_angle - start_angle;
 		ccw_angle = 360.0f - end_angle + start_angle;
 	}
-	else {	// start_angle > end_angle
+	else if (start_angle > end_angle) {
 		cw_angle = 360.0f - start_angle + end_angle;
 		ccw_angle = start_angle - end_angle;
 	}
+	else {
+		return NO_ROT;
+	}
 
-	return cw_angle > ccw_angle ? -1 : 1;
+	return cw_angle > ccw_angle ? CCW : CW;
 }
 
 #ifdef __cplusplus
