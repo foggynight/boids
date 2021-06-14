071954e16712a1f7a6ffd986f4d35974e447aec4
foggynight <foggynight709@gmail.com>
2021-06-13 23:31:19 -0230

    Update test_Entity

--- tests/test_Entity.cpp
+++ tests/test_Entity.cpp
@@ -32,10 +32,6 @@ int main()
 
 void test_constructor()
 {
-	/**
-	 * Single test case as constructor currently does not check bounds.
-	 **/
-
 	const float start_x = 0.0f;
 	const float start_y = 1.0f;
 	const float start_angle = 2.0f;
@@ -51,40 +47,19 @@ void test_constructor()
 
 void test_rotate()
 {
-	/**
-	 * Test case format: start_angle, delta_angle, expected_result
-	 **/
-
 	Entity entity(Vec2(0.0f, 0.0f), 0.0f, 0.0f);
+	auto test = [&entity](float start_angle, float delta_angle, float new_angle) {
+		entity.angle = start_angle;
+		entity.rotate(delta_angle);
+		assert(approx_equal(entity.angle, new_angle));
+	};
 
-	// Test 0: 0, 0, 0
-	entity.rotate(0.0f);
-	assert(approx_equal(entity.angle, 0.0f));
-
-	// Test 1: 0, 90, 90
-	entity.angle = 0.0f;
-	entity.rotate(90.0f);
-	assert(approx_equal(entity.angle, 90.0f));
-
-	// Test 2: 0, -90, 270
-	entity.angle = 0.0f;
-	entity.rotate(-90.0f);
-	assert(approx_equal(entity.angle, 270.0f));
-
-	// Test 3: 270, 90, 0
-	entity.angle = 270.0f;
-	entity.rotate(90.0f);
-	assert(approx_equal(entity.angle, 0.0f));
-
-	// Test 4: 0, 450, 90
-	entity.angle = 0.0f;
-	entity.rotate(450.0f);
-	assert(approx_equal(entity.angle, 90.0f));
-
-	// Test 5: 0, -450, 270
-	entity.angle = 0.0f;
-	entity.rotate(-450.0f);
-	assert(approx_equal(entity.angle, 270.0f));
+	test(0, 0, 0);
+	test(0, 90, 90);
+	test(0, -90, 270);
+	test(270, 90, 0);
+	test(0, 450, 90);
+	test(0, -450, 270);
 }
 
 void test_rotate_towards()
@@ -94,131 +69,75 @@ void test_rotate_towards()
 
 void test_update_pos()
 {
-	/**
-	 * Test case format:
-	 * start_x, start_y, angle, velocity, expected_x, expected_y
-	 *
-	 * These test cases assume the entity travels for one second.
-	 **/
-
 	Entity entity(Vec2(0.0f, 0.0f), 0.0f, 0.0f);
+	auto test = [&entity](
+			float x, float y,
+			float angle, float velocity,
+			float new_x, float new_y)
+	{
+		entity.pos.x = x;
+		entity.pos.y = y;
+		entity.angle = angle;
+		entity.velocity = velocity;
+		entity.update_pos(1.0f * US_PER_SECOND);
+		assert(approx_equal(entity.pos.x, new_x));
+		assert(approx_equal(entity.pos.y, new_y));
+	};
 
-	// Test 0: 0, 0, 0, 0, 0, 0
-	entity.update_pos(1.0f * US_PER_SECOND);
-	assert(approx_equal(entity.pos.x, 0.0f));
-	assert(approx_equal(entity.pos.y, 0.0f));
-
-	// Test 1: 0, 0, 0, 1, 1, 0
-	entity.pos.x = 0.0f;
-	entity.pos.y = 0.0f;
-	entity.angle = 0.0f;
-	entity.velocity = 1.0f;
-	entity.update_pos(1.0f * US_PER_SECOND);
-	assert(approx_equal(entity.pos.x, 1.0f));
-	assert(approx_equal(entity.pos.y, 0.0f));
-
-	// Test 2: 0, 0, 45, sqrt(2), 1, 1
-	entity.pos.x = 0.0f;
-	entity.pos.y = 0.0f;
-	entity.angle = 45.0f;
-	entity.velocity = sqrt(2);
-	entity.update_pos(1.0f * US_PER_SECOND);
-	assert(approx_equal(entity.pos.x, 1.0f));
-	assert(approx_equal(entity.pos.y, 1.0f));
-
-	// Test 3: 1, 0, 180, 1, 0, 0
-	entity.pos.x = 1.0f;
-	entity.pos.y = 0.0f;
-	entity.angle = 180.0f;
-	entity.velocity = 1.0f;
-	entity.update_pos(1.0f * US_PER_SECOND);
-	assert(approx_equal(entity.pos.x, 0.0f));
-	assert(approx_equal(entity.pos.y, 0.0f));
-
-	// Test 4: 1, 0, 135, sqrt(2), 0, 1
-	entity.pos.x = 1.0f;
-	entity.pos.y = 0.0f;
-	entity.angle = 135.0f;
-	entity.velocity = sqrt(2);
-	entity.update_pos(1.0f * US_PER_SECOND);
-	assert(approx_equal(entity.pos.x, 0.0f));
-	assert(approx_equal(entity.pos.y, 1.0f));
-
-	// Test 5: 0, 0, 90, 1, 0, 1
-	entity.pos.x = 0.0f;
-	entity.pos.y = 0.0f;
-	entity.angle = 90.0f;
-	entity.velocity = 1.0f;
-	entity.update_pos(1.0f * US_PER_SECOND);
-	assert(approx_equal(entity.pos.x, 0.0f));
-	assert(approx_equal(entity.pos.y, 1.0f));
-
-	// Test 6: 1, 1, 225, sqrt(2), 0, 0
-	entity.pos.x = 1.0f;
-	entity.pos.y = 1.0f;
-	entity.angle = 225.0f;
-	entity.velocity = sqrt(2);
-	entity.update_pos(1.0f * US_PER_SECOND);
-	assert(approx_equal(entity.pos.x, 0.0f));
-	assert(approx_equal(entity.pos.y, 0.0f));
-
-	// Test 7: 0, 1, 270, 1, 0, 0
-	entity.pos.x = 0.0f;
-	entity.pos.y = 1.0f;
-	entity.angle = 270.0f;
-	entity.velocity = 1.0f;
-	entity.update_pos(1.0f * US_PER_SECOND);
-	assert(approx_equal(entity.pos.x, 0.0f));
-	assert(approx_equal(entity.pos.y, 0.0f));
-
-	// Test 8: 0, 1, 315, sqrt(2), 1, 0
-	entity.pos.x = 0.0f;
-	entity.pos.y = 1.0f;
-	entity.angle = 315.0f;
-	entity.velocity = sqrt(2);
-	entity.update_pos(1.0f * US_PER_SECOND);
-	assert(approx_equal(entity.pos.x, 1.0f));
-	assert(approx_equal(entity.pos.y, 0.0f));
+	test(0, 0, 0, 0, 0, 0);
+	test(0, 0, 0, 1, 1, 0);
+	test(0, 0, 45, sqrt(2), 1, 1);
+	test(1, 0, 180, 1, 0, 0);
+	test(1, 0, 135, sqrt(2), 0, 1);
+	test(0, 0, 90, 1, 0, 1);
+	test(1, 1, 225, sqrt(2), 0, 0);
+	test(0, 1, 270, 1, 0, 0);
+	test(0, 1, 315, sqrt(2), 1, 0);
 }
 
 void test_in_fov()
 {
-	/**
-	 * Test case format:
-	 * entity0.pos.x, entity0.pos.y, entity0_angle, entity1.pos.x, entity1.pos.y, expected_result
-	 **/
+	const float initial_fov_radius = Entity::get_fov_radius();
+	const float initial_fov_max_angle = Entity::get_fov_max_angle();
+
+	const float fov_radius = 100.0f;
+	const float fov_max_angle = 120.0f;
+
+	Entity::set_fov_radius(fov_radius);
+	Entity::set_fov_max_angle(fov_max_angle);
 
 	Entity entity0(Vec2(0.0f, 0.0f), 0.0f, 0.0f);	// Looker
 	Entity entity1(Vec2(0.0f, 0.0f), 0.0f, 0.0f);	// Target
+	auto test = [&entity0, &entity1](
+			float x0, float y0, float angle0,
+			float x1, float y1,
+			bool is_in_fov)
+	{
+		entity0.pos.x = x0;
+		entity0.pos.y = y0;
+		entity0.angle = angle0;
+		entity1.pos.x = x1;
+		entity1.pos.y = y1;
+		assert(entity0.in_fov(entity1) == is_in_fov);
+	};
 
-	const float fov_radius = entity0.get_fov_radius();
-	const float fov_max_angle = entity0.get_fov_max_angle();
+	test(0, 0, 0, 0, 0, true);
+	test(0, 0, 180, 0, 0, false);
+	test(0, 0, 0, fov_radius-1, 0, true);
+	test(0, 0, 0, fov_radius, 0, true);
+	test(0, 0, 0, fov_radius+1, 0, false);
+	test(0, 0, 0, 0, fov_radius-1, true);
+	test(0, 0, 0, 0, fov_radius, true);
+	test(0, 0, 0, 0, fov_radius+1, false);
+	test(0, 0, 0, fov_radius/2, fov_radius/2, true);
+	test(0, 0, 0, fov_radius, fov_radius, false);
+	test(0, 0, fov_max_angle-1, fov_radius, 0, true);
+	test(0, 0, fov_max_angle, fov_radius, 0, true);
+	test(0, 0, fov_max_angle+1, fov_radius, 0, false);
+	test(0, 0, 360 - fov_max_angle + 1, fov_radius, 0, true);
+	test(0, 0, 360 - fov_max_angle, fov_radius, 0, true);
+	test(0, 0, 360 - fov_max_angle - 1, fov_radius, 0, false);
 
-	// Test 0: 0, 0, 0, 0, 0, true
-	assert(entity0.in_fov(entity1) == true);
-
-	// Test 1: 0, 0, 180, 0, 0, false
-	entity0.angle = 180.0f;
-	assert(entity0.in_fov(entity1) == false);
-
-	// @TODO Add other test cases
-	// Test 2: 0, 0, 0, 1, 0, true
-	entity0.pos.x = 0.0f;
-	entity0.pos.y = 0.0f;
-	entity0.angle = 0.0f;
-	entity1.pos.x = 1.0f;
-	entity1.pos.y = 0.0f;
-	assert(entity0.in_fov(entity1) == true);
-
-	// Test 3: 0, 0, 0, 0, 0, true
-
-	// Test 4: 0, 0, 0, 0, 0, true
-
-	// Test 5: 0, 0, 0, 0, 0, true
-
-	// Test 6: 0, 0, 0, 0, 0, true
-
-	// Test 7: 0, 0, 0, 0, 0, true
-
-	// Test 8: 0, 0, 0, 0, 0, true
+	Entity::set_fov_radius(initial_fov_radius);
+	Entity::set_fov_max_angle(initial_fov_max_angle);
 }
