d8b2feee09eb1b1e193532aa8fec00ded4d83026
foggynight <foggynight709@gmail.com>
2021-06-18 17:58:48 -0230

    Get the boids moving

--- boids.lisp
+++ boids.lisp
@@ -15,31 +15,31 @@
 
 ;;; CONFIG SECTION -------------------------------------------------------------
 
-;; Number of boids to simulate
+;; Number of boids to simulate.
 (defparameter *boid-count* 32)
 
-;; Radius of the circle containing a boid
+;; Radius of the circle containing a boid.
 (defparameter *boid-radius* 16)
 ;; Angle between the two vectors pointing from the center of a boid to its left
-;; and right tail points
+;; and right tail points.
 (defparameter *boid-tail-angle* 60)
 
-;; Screen width and height
+;; Screen width and height.
 (defparameter *sw* 1280)
 (defparameter *sh* 720)
 
 ;;; UTILITY SECTION ------------------------------------------------------------
 
-;; Convert radians to degrees
+;; Convert radians to degrees.
 (defun degrees (radians)
   (* (/ radians pi) 180.0))
 
-;; Convert degrees to radians
+;; Convert degrees to radians.
 (defun radians (degrees)
   (* (/ degrees 180.0) pi))
 
 ;; Convert an x-y vector into an angle in degrees relative to the positive
-;; x-axis with a positive rotation
+;; x-axis with a positive rotation.
 (defun vec2-to-angle (x y)
   (let* ((angle 0))
     (if (= x 0)
@@ -54,18 +54,24 @@
 
 ;;; BOID SECTION ---------------------------------------------------------------
 
-;; @TODO Write a comment for this
+;; Boid class representing a boid in the world. Boid's have a position and
+;; velocity, both with an x and y component.
 (defclass boid ()
   ((x :accessor boid-x)
    (y :accessor boid-y)
    (dx :accessor boid-dx)
    (dy :accessor boid-dy)))
 
-;; Determine the angle of a boid based on its velocity
+;; Determine the angle of a boid based on its velocity.
 (defmethod boid-angle ((object boid))
   (vec2-to-angle (boid-dx object) (boid-dy object)))
 
-;; Initialize a list of boids with random parameters
+;; Update the position of a boid based on its velocity.
+(defmethod boid-update-pos ((object boid))
+  (setf (boid-x object) (+ (boid-x object) (boid-dx object)))
+  (setf (boid-y object) (+ (boid-y object) (boid-dy object))))
+
+;; Initialize a list of boids with random parameters.
 (defun boid-init (boid-count)
   (let ((boid nil)
         (boid-list nil))
@@ -73,22 +79,27 @@
       (setq boid (make-instance 'boid))
       (setf (boid-x boid) (random *sw*))
       (setf (boid-y boid) (random *sh*))
-      ;; @TODO Figure out how to handle initial velocity
-      (setf (boid-dx boid) (- (random 9) 4))
-      (setf (boid-dy boid) (- (random 9) 4))
+      ;; Random velocity with components in the range:
+      ;; { v:real | -1 <= v <= 1 and v * 10 == floor(v * 10) }
+      (setf (boid-dx boid) (/ (- (random 21) 10) 10.0))
+      (setf (boid-dy boid) (/ (- (random 21) 10) 10.0))
       (setq boid-list (cons boid boid-list)))
     boid-list))
 
-;; @TODO boid-update
+;; Update the positions of a list of boids.
+(defun boid-list-update-pos (boid-list)
+  (unless (eq boid-list nil)
+    (boid-update-pos (car boid-list))
+    (boid-list-update-pos (cdr boid-list))))
 
 ;;; RENDER SECTION -------------------------------------------------------------
 
-;; Clear the screen
+;; Clear the screen.
 (defun render-clear (ren)
   (sdl2:set-render-draw-color ren 0 0 0 255)
   (sdl2:render-clear ren))
 
-;; Draw a boid
+;; Draw a boid.
 (defun render-draw-boid (ren boid)
   (let* ((x (boid-x boid))
          (y (boid-y boid))
@@ -104,11 +115,11 @@
     (sdl2:render-draw-line ren forward-x forward-y right-x right-y)
     (sdl2:render-draw-line ren left-x left-y right-x right-y)))
 
-;; Draw a list of boids
-(defun render-draw-boids (ren boid-list)
+;; Draw a list of boids.
+(defun render-draw-boid-list (ren boid-list)
   (unless (eq boid-list nil)
     (render-draw-boid ren (car boid-list))
-    (render-draw-boids ren (cdr boid-list))))
+    (render-draw-boid-list ren (cdr boid-list))))
 
 ;;; MAIN SECTION ---------------------------------------------------------------
 
@@ -125,7 +136,8 @@
             (:idle
              ()
              (render-clear ren)
-             (render-draw-boids ren boid-list)
+             (render-draw-boid-list ren boid-list)
              (sdl2:render-present ren)
-             (sdl2:delay 5))
+             (boid-list-update-pos boid-list)
+             (sdl2:delay 7)) ; 7 ms ~= 1000 ms / 144 fps
             (:quit () t)))))))
